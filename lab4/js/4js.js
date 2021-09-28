2222b5caa128237cf4994695c63e0311f7137c33
KoTKoKoC <87563305+KoTKoKoC@users.noreply.github.com>
2021-09-28 14:40:26 +0700

    Update 4js.js

--- lab4/js/4js.js
+++ lab4/js/4js.js
@@ -1,35 +1,12 @@
-    document.addEventListener('DOMContentLoaded', () => {
-    const input = document.getElementById('input');
+   const word = "12qweyo",
+  vowel = [],
+  consonants = [];
 
-    const button = document.getElementById('btn');
+for (let char of word) {
+  if (char.match(/[aeiou]/)) vowel.push(char);
+  else consonants.push(char);
 
-    const listVow = document.getElementById('listVow-result');
-    const listNVow = document.getElementById('listNVow-result');
+}
 
-    // АЛГОРИТМ
-  const countChars = (string) => {
-  if (typeof string !== 'string') {
-  throw new Error("Введена не строка!");
-        } 
-  
-  listVow = [],
-  listNVow = [];
-
-  for (let char of countChars) {
-  if (char.match(/[aeiou]/)) listVow.push(char);
-  else listNVow.push(char);
-      
-
-}}
-      
-  button.addEventListener('click', () => {
-        if (!input.value) {
-            listVow.innerHTML = "Пустая строка"
-            listNVow.innerHTML = "Пустая строка"
-            return;
-        }     
- const chars = countChars(input.value);
- listVow.innerHTML = chars.listVow
- listNVow.innerHTML = chars.listNVow
-})
-
+writeln(vowel);
+writeln(consonants);
