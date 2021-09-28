029b5ae6644960cee4ed2203e5bcf2dbd0fe7d72
KoTKoKoC <87563305+KoTKoKoC@users.noreply.github.com>
2021-09-28 13:34:27 +0700

    Update 4js.js

--- lab4/js/4js.js
+++ lab4/js/4js.js
@@ -1,13 +1,23 @@
 
+    package com.javarush.task.task09.task0923;    
+    document.addEventListener('DOMContentLoaded', () => {
+    const input = document.getElementById('input');
+
+    const button = document.getElementById('btn');
+
+    const listVow = document.getElementById('letters-result');
+    const listNVow = document.getElementById('decimal-result');
+
     // АЛГОРИТМ
-   public class Solution {
-    public static char[] vowels = new char[]{'а', 'я', 'у', 'ю', 'и', 'ы', 'э', 'е', 'о', 'ё'};
-    public static void main(String[] args) throws Exception {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        char[] chArr = reader.readLine().toCharArray();//создаем массив, куда поместим строку считанную с консоли,которую переводим в char
-        ArrayList<Character> listVow = new ArrayList<Character>();//список для гласных
-        ArrayList<Character> listNVow = new ArrayList<Character>();//список для негласных
-        for (int i = 0; i < chArr.length; i++) {//бежим по массиву
+    
+        public class Solution {
+        public static char[] vowels = new char[]{'а', 'я', 'у', 'ю', 'и', 'ы', 'э', 'е', 'о', 'ё'};
+
+        let listVowCount = 0;
+        let listNVowCount = 0;
+
+        const countChars = string.split("");
+         for (int i = 0; i < chArr.length; i++) {//бежим по массиву
             if (isVowel(chArr[i])) {//проверяем условие методом isVowel если текуший символ гласная
                 listVow.add(chArr[i]); //помещаем гласную в свой список
             } else if (chArr[i] == ' ') {// проверяем есть ли пробел, и пропускаем его
@@ -15,28 +25,29 @@
             } else {
                 listNVow.add(chArr[i]);//остальные буквы или другие символы(точка например), помещаем в свой список
             }
-        }
-        for (char c : listVow) {//бежим по списку, в который положили гласные
-            System.out.print(c + " ");//выводи гланые на экран, не забываем пробел между ними
-        }
-        System.out.println();// переводим курсов на другую строчку
-        for (char c : listNVow) {//бежим по списку согласных
-            System.out.print(c + " ");//выводим на экран все остальные символы, через пробел
-        }
+       for (char input : listVow) {};
+       for (char input : listNVow) {};
+
+       
     }
-
-
-
-    // метод проверяет, гласная ли буква
+    // 
+     // метод проверяет, гласная ли буква
     public static boolean isVowel(char c) {
-        c = Character.toLowerCase(c);  // приводим символ в нижний регистр - от заглавных к строчным буквам
+        input = Character.toLowerCase(c);  // приводим символ в нижний регистр - от заглавных к строчным буквам
 
-        for (char d : vowels)   // ищем среди массива гласных
+        for (char input : vowels)   // ищем среди массива гласных
         {
-            if (c == d)
+            if (input == input)
                 return true;
         }
         return false;
     }
 }
    
+
+        const chars = countChars(input.value);
+
+        listVow.innerHTML = chars.listVow
+        listNVow.innerHTML = chars.listNVow
+    })
+})
