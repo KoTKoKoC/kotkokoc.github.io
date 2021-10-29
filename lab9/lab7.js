6f3bcd2b325243185ff204bc470d220025b3bc09
KoTKoKoC <87563305+KoTKoKoC@users.noreply.github.com>
2021-10-29 15:40:25 +0700

    Add files via upload

---
+++ lab9/lab7.js
@@ -0,0 +1,31 @@
+var mas1 = new Array()
+var mas2 = new Array()
+for (var i = 0; i < 2; i++) {
+	mas1[i] = prompt("Введите координату дружественого ферзя")
+	mas2[i] = prompt("Введите координату вражеского ферзя")
+}
+
+function CountingNumCheck(num) {
+    if ( (! /[^1-8]/g.test(num)) && (typeof(num) == "string") && (num.indexOf('0')!=0) )
+        return true
+    else return false
+}
+
+var k = 0
+for (var i = 0; i < 2; i++) {
+	if ( !CountingNumCheck(mas1[i]) || !CountingNumCheck(mas2[i]))
+		k = k+1
+	else if (!Array.isArray(mas1) || !Array.isArray(mas2))
+		k = k+1
+	else if (mas1[0]==mas2[0] && mas1[1]==mas2[1])
+		k = k+1
+}
+
+if (k == 0){
+ if( ( (mas1[0]==mas2[0]) && (mas1[1]!=mas2[1]) )|| ((mas1[0]!=mas2[0])&&(mas1[1]==mas2[1])) 
+ 	|| (Math.abs(mas1[0]-mas2[0])==Math.abs(mas1[1]-mas2[1])) )
+        alert("YES")
+ 
+    else
+        alert("NO")
+} else alert("Вы ввели неверные данные")
